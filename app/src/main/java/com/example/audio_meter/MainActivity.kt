e820cd1dac824d33356302ab8b423ade08e998c7
Daniel <d.brummerloh@gmail.com>
2023-08-31 23:28:30 +0200

    cleanup

--- app/src/main/java/com/example/audio_meter/MainActivity.kt
+++ app/src/main/java/com/example/audio_meter/MainActivity.kt
@@ -23,25 +23,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-class Test : ComponentActivity() {
-    private var webView: WebView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        webView = WebView(this)
-        setContentView(webView)
-        webView!!.settings.javaScriptEnabled = true
-        webView!!.loadUrl("https://www.example.com")
-
-        // Set up WebViewClient to handle page navigation within the WebView
-        webView!!.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                view.loadUrl(url)
-                return true
-            }
-        }
-    }
 
     override fun onPause() {
         super.onPause()
