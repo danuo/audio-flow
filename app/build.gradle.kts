caea30853367841cb33e026b6573aa84871b344d
Daniel <d.brummerloh@gmail.com>
2023-09-07 01:05:32 +0200

    remove unused dependencies and theme classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,13 +63,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.ui:ui-graphics:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
-    implementation("androidx.compose.material3:material3:1.1.1")
+    // implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    // implementation("androidx.compose.material3:material3:1.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -79,26 +77,32 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+
+    // activity
+    // implementation("androidx.activity:activity-compose:1.7.2")
+    // implementation("androidx.activity:activity-ktx:1.7.2")
 
     // Room components
+    implementation("androidx.room:room-runtime:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     ksp("androidx.room:room-compiler:2.5.2")
     androidTestImplementation("androidx.room:room-testing:2.5.2")
 
     // Lifecycle components
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
+    // implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    // implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    // implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
 
     // Kotlin components
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
+    // api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    // api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // UI
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.9.0")
+    // implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // implementation("com.google.android.material:material:1.9.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     // Server
